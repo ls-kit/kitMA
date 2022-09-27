0504f90a0c296bbcc737a5ed131881733879f8f8
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-27 14:42:13 +0600

    slider done

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ $('.btn-inactive').click(function () {
     $('.btn-inactive').removeClass('active');
     $(this).addClass('active');
 })
+
+$('.sliderr').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true
+});
